e16e77d3bcd9036024f19cbabb0d4b108d3cfd5d
beafromsea <beatrizdoots@gmail.com>
2023-10-14 21:15:15 -0300

    feat: :sparkles: Importação do arquivo app.jsx e GlobalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import { StyleContainer } from "./style.styles.js";
-
-
+import GlobalStyles from './styles/GlobalStyles.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StyleContainer>
       <App />
     </StyleContainer>
+    <GlobalStyles/>
   </React.StrictMode>,
 )
